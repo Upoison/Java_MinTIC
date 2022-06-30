308c66adc81698df9a0dde359ee4983bf0df8f07
upoison <destivengarzon@gmail.com>
2022-06-29 22:37:24 -0500

    Adding new files about Java

---
+++ Ejemplo1/src/Constructor.java
@@ -0,0 +1,14 @@
+public class Constructor {
+    int edad;
+    String nombre;
+
+    public Constructor() { // el nombre debe conincidir con el nombre de la clase, no puede tener ningun retorno
+        edad = 20;
+        nombre = "Pedro";
+    }
+
+    public static void main(String[] args){
+        Constructor
+        
+    }
+}
---
+++ Ejemplo1/src/Interes.java
@@ -0,0 +1,12 @@
+public class Interes {
+    public static void main(String[] args) throws Exception {
+        double pMonto = 100;
+        double pInteres = 3;
+        int pTiempo = 6;
+        double intS;
+        double intC;
+        double compI;
+        intS =Math.round pMonto * (pInteres/100) * pTiempo;
+
+    }
+}

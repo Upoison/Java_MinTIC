20e802e46b182e1a09b0fe60a54d473bc2e5bb4f
upoison <destivengarzon@gmail.com>
2022-07-12 13:47:36 -0500

    Adding Reto 2Java

---
+++ reto2java/demo/src/main/java/com/examplereto2/Computadores.java
@@ -0,0 +1,30 @@
+package com.examplereto2;
+
+public class Computadores {
+    //Atributos:
+    protected final static char ConsumoW
+    protected Double  {
+        
+    }
+
+    public Computadores() {
+        this.precioBase = PRECIO_BASE;
+        this.peso = PESO_BASE;
+        this.ConsumoW = CONSUMO_W;
+        //this(PRECIO_BASE, PESO_BASE, CONSUMO_W; // Llamma al constructor de la misma clase
+    }
+    public Computadores(Double precioBase, Integer peso) {
+
+    }
+    public Computadores(Double precioBase, Integer peso, char ConsumoW) {
+        this.precioBase = precioBase;
+        this.peso = peso;
+        this.consumoW = consumoW;
+
+    }
+    //Metodos
+    public Double calcularPrecio(){
+        Double adicion = 0.0;
+    }
+}
+
